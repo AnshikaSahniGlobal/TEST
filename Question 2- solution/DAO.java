30352a55bbac915ad182c430e3111c361861069c
Anshika sahni <110605894+AnshikaSahniGlobal@users.noreply.github.com>
2022-08-08 16:39:33 +0530

    ASSIGNMENT1

--- Question 2- solution/DAO.java
+++ Question 2- solution/DAO.java
@@ -1,14 +1,14 @@
-package jdbcCalls;
 
-import java.sql.Connection;
+
+import java.sql.Connection; 
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DAO {
-	static Connection con;
-	static PreparedStatement ps;
+	static Connection con=null;
+	static PreparedStatement p=null;
 	static {
 		
 		 try {
@@ -25,49 +25,59 @@ public class DAO {
 		}
  
 	}
+	public static boolean insert (int rollno,String name,String std,String dob ,double fee)throws SQLException
+	{
+		String query = "insert into student values(?, ?, ?, ?, ?)";
+		  PreparedStatement p= con.prepareStatement(query);
+		  p.setInt(1, rollno);
+		  p.setString(2, name);
+		  p.setString(3, std);
+		  p.setString(4, dob);
+		  p.setDouble(5, fee);
+		  p.executeUpdate();
+		  return true;
+	}
 	
-
 	
-	public static boolean display(int rollno) throws SQLException {
+   public static boolean display(int rollno)throws SQLException {
 		
 		String query = "select * from student where rollno = ?";
 		
-		ps = con.prepareStatement(query);
+		p = con.prepareStatement(query);
 		
-		ps.setInt(1,rollno);
-		ResultSet rs = ps.executeQuery();
+		p.setInt(1,rollno);
+		ResultSet rs = p.executeQuery();
 		
 		while(rs.next()) {
 			int roll = rs.getInt("RollNo");
-			String name = rs.getString("name");
+			String name = rs.getString("studentname");
 			String std = rs.getString("standard");
 			String dob = rs.getString("Date_of_Birth");
-			double fee = rs.getDouble("fees");
+			double fee = rs.getDouble("fee");
 			
 			System.out.format("%15s %15s %15s %15s %15s\n", roll, name, std, dob, fee);
 		}
 		
 		return true;
 	}
-	
-	public static boolean displayAll() throws SQLException {
-		
+    public static boolean displayAll() throws SQLException{
+
 		String query = "select * from student";
 		
-		ps = con.prepareStatement(query);
-		ResultSet rs = ps.executeQuery();
+		p = con.prepareStatement(query);
+		ResultSet rs = p.executeQuery();
 		
 		while(rs.next()) {
 			int roll = rs.getInt("RollNo");
-			String name = rs.getString("name");
+			String name = rs.getString("studentname");
 			String std = rs.getString("standard");
 			String dob = rs.getString("Date_of_Birth");
-			double fee = rs.getDouble("fees");
+			double fee = rs.getDouble("fee");
 			
 			System.out.format("%15s %15s %15s %15s %15s\n", roll, name, std, dob, fee);
 		}
 		
 		return true;
-	}
+     }
 	
 }
