f62706d8725e26f1591acb720c54b83658475b28
AnshikaSahniGlobal <84393963+AnshikaSahni@users.noreply.github.com>
2022-08-22 10:25:17 +0530

    "WeeklyTest"

---
+++ Assignment3-22-August/Question2-solution/SpringWithRestfulServices/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into customer values (101,'anshika','203040','sampurna nagar','anshika123@gmail','1234');
+insert into customer values (102,'jitin','203041','punjab','jitin123@gmail','12345');
+insert into customer values (103,'mummy','203042','sampurna nagar','mummy123@gmail','123456');
+insert into customer values (104,'pranjali','203043','palia','pranjali123@gmail','1234567');
